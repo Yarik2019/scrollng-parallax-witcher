a0200e91e11ea4a709a802d1e496a9ba1839a903
Yarik2019 <yroslavterno@gmail.com>
2023-07-10 13:21:20 +0300

    dsada

---
+++ js/java.js
@@ -0,0 +1,12 @@
+new Swiper('.slider', {
+    // direction: 'vertical',
+	// freeMode: true,
+    speed: 2400,
+	parallax: true,
+	spaceBetween: 18,
+    mousewheel: true
+ 	// mousewheel: {
+	// 	enabled: true,
+	// 	sensitivity: 7.4
+	// },
+})
